8937300cbeaca48437eaaa60e46acd0e15ee020c
md-zeon <zeonrahaman5870@gmail.com>
2025-07-20 13:59:18 +0600

    Enhanced Design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,6 +25,62 @@ const Navbar = () => {
 		document.documentElement.setAttribute("data-theme", storedTheme);
 	}, []);
 
+	const ThemeController = (
+		<>
+			<label className='toggle text-base-content'>
+				<input
+					type='checkbox'
+					onChange={handleThemeToggle}
+					checked={theme === "dark"}
+				/>
+
+				<svg
+					aria-label='sun'
+					xmlns='http://www.w3.org/2000/svg'
+					viewBox='0 0 24 24'
+				>
+					<g
+						strokeLinejoin='round'
+						strokeLinecap='round'
+						strokeWidth='2'
+						fill='none'
+						stroke='currentColor'
+					>
+						<circle
+							cx='12'
+							cy='12'
+							r='4'
+						></circle>
+						<path d='M12 2v2'></path>
+						<path d='M12 20v2'></path>
+						<path d='m4.93 4.93 1.41 1.41'></path>
+						<path d='m17.66 17.66 1.41 1.41'></path>
+						<path d='M2 12h2'></path>
+						<path d='M20 12h2'></path>
+						<path d='m6.34 17.66-1.41 1.41'></path>
+						<path d='m19.07 4.93-1.41 1.41'></path>
+					</g>
+				</svg>
+
+				<svg
+					aria-label='moon'
+					xmlns='http://www.w3.org/2000/svg'
+					viewBox='0 0 24 24'
+				>
+					<g
+						strokeLinejoin='round'
+						strokeLinecap='round'
+						strokeWidth='2'
+						fill='none'
+						stroke='currentColor'
+					>
+						<path d='M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z'></path>
+					</g>
+				</svg>
+			</label>
+		</>
+	);
+
 	const navLinks = (
 		<>
 			<li>
@@ -140,57 +196,7 @@ const Navbar = () => {
 										</div>
 										<div className='flex justify-between items-center'>
 											<span className='badge bg-gradient'>Worker</span>
-											<label className='toggle text-base-content'>
-												<input
-													type='checkbox'
-													onChange={handleThemeToggle}
-													checked={theme === "dark"}
-												/>
-
-												<svg
-													aria-label='sun'
-													xmlns='http://www.w3.org/2000/svg'
-													viewBox='0 0 24 24'
-												>
-													<g
-														strokeLinejoin='round'
-														strokeLinecap='round'
-														strokeWidth='2'
-														fill='none'
-														stroke='currentColor'
-													>
-														<circle
-															cx='12'
-															cy='12'
-															r='4'
-														></circle>
-														<path d='M12 2v2'></path>
-														<path d='M12 20v2'></path>
-														<path d='m4.93 4.93 1.41 1.41'></path>
-														<path d='m17.66 17.66 1.41 1.41'></path>
-														<path d='M2 12h2'></path>
-														<path d='M20 12h2'></path>
-														<path d='m6.34 17.66-1.41 1.41'></path>
-														<path d='m19.07 4.93-1.41 1.41'></path>
-													</g>
-												</svg>
-
-												<svg
-													aria-label='moon'
-													xmlns='http://www.w3.org/2000/svg'
-													viewBox='0 0 24 24'
-												>
-													<g
-														strokeLinejoin='round'
-														strokeLinecap='round'
-														strokeWidth='2'
-														fill='none'
-														stroke='currentColor'
-													>
-														<path d='M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z'></path>
-													</g>
-												</svg>
-											</label>
+											{ThemeController}
 										</div>
 										<div>
 											<button
@@ -206,6 +212,7 @@ const Navbar = () => {
 							</>
 						) : (
 							<>
+								{ThemeController}
 								<Link
 									to='/login'
 									className='btn btn-ghost'
