241d7fe70bd43e4bcdb0a0f24f6244a6577bf2eb
md-zeon <zeon.cse@gmail.com>
2026-07-04 23:03:23 +0600

    feat: replace toast (sonner), dialogs (alert-dialog), remove sweetalert2/aos/react-hot-toast/daisyui, overhaul theme system with shadcn CSS vars, update font to Inter

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,27 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import "aos/dist/aos.css";
 import { RouterProvider } from "react-router";
 import router from "./Routes/Routes.jsx";
 import AuthProvider from "./providers/AuthProvider.jsx";
-import { Toaster } from "react-hot-toast";
+import { Toaster } from "sonner";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HelmetProvider } from "react-helmet-async";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 // Create a client
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
-	<StrictMode>
-		<HelmetProvider>
-			<QueryClientProvider client={queryClient}>
-				<AuthProvider>
-					<RouterProvider router={router} />
-				</AuthProvider>
-			</QueryClientProvider>
-		</HelmetProvider>
-		<Toaster
-			position='top-right'
-			reverseOrder={false}
-		/>
-	</StrictMode>,
+  <StrictMode>
+    <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <TooltipProvider>
+            <RouterProvider router={router} />
+          </TooltipProvider>
+        </AuthProvider>
+      </QueryClientProvider>
+    </HelmetProvider>
+    <Toaster position="top-right" richColors closeButton />
+  </StrictMode>,
 );
